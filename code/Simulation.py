05d70c1ace130dd504bdded7b3cba17d8c88b3cc
hkneiding <k.1996@live.de>
2019-03-31 12:22:30 +0200

    Minor fixes, results

--- code/Simulation.py
+++ code/Simulation.py
@@ -26,19 +26,21 @@ class Simulation:
         # set up systems
         current_system = copy.deepcopy(self.initial_system)
         current_system.calculate_wave_function(alpha)
-        #trial_system = copy.deepcopy(current_system)
 
         # initialize averages
         avg = Observables()
 
         for i in range(mc_iterations):
 
+            if (i+1) % 1000 == 0:
+                print("Iteration " + str(i+1) + ", " + str(round((i+1)/mc_iterations * 100, 2)) + "%")
+
             if use_importance_sampling:
                 current_system.calculate_drift_force(alpha)
 
             current_system, acceptance_rate = self.mc_step(alpha, current_system, copy.deepcopy(current_system),
-                                                           use_importance_sampling, update_radius=update_radius,
-                                                           time_step=time_step)
+                                                                 use_importance_sampling, update_radius=update_radius,
+                                                                 time_step=time_step)
 
             # calculate local energy of last configuration
             current_system.calculate_local_energy(alpha)
@@ -50,9 +52,11 @@ class Simulation:
             avg.update_cumulative_quantities(current_system.local_energy, current_system.wave_function_value,
                                              current_system.wave_function_derivative)
 
+            avg.positions.append(current_system.particles[0].position.tolist())
+
         # finalize averages
         avg.finalize_averages(mc_iterations)
-        avg.final_configuration = current_system.get_particle_position_array()
+
         return avg
 
     def mc_step(self, alpha, current_system, trial_system, use_importance_sampling=False, update_radius=1,
@@ -93,6 +97,48 @@ class Simulation:
             else:
                 for k in range(len(self.initial_system.particles[j].position)):
                     trial_system.particles[j].position[k] = current_system.particles[j].position[k]
+
+        return current_system, accepted_steps
+
+    def mc_step_short(self, alpha, current_system, trial_system, use_importance_sampling=False, update_radius=1,
+                      time_step=0.001):
+
+        accepted_steps = 0
+
+        # randomly select a particle
+        j = np.random.randint(low=0, high=current_system.particle_number)
+
+        # generate trial configuration
+        if use_importance_sampling:
+            trial_system.particles[j].perturb_position_importance(current_system.drift_force[j, :],
+                                                                  time_step=time_step)
+            trial_system.calculate_drift_force(alpha)
+        else:
+            trial_system.particles[j].perturb_position_uniformly(update_radius)
+
+        # calculate wave function of trial configuration
+        trial_system.calculate_wave_function(alpha)
+
+        # calculate acceptance probability
+        acceptance_probability = trial_system.wave_function_value ** 2 / current_system.wave_function_value ** 2
+
+        # multiply by greens function if using importance sampling
+        if use_importance_sampling:
+            acceptance_probability *= self.evaluate_greens_function(current_system.particles[j].position,
+                                                                    trial_system.particles[j].position,
+                                                                    current_system.drift_force[j, :],
+                                                                    trial_system.drift_force[j, :],
+                                                                    time_step=time_step)
+
+        # update configuration or roll back changes in trial configuration
+        if Simulation.check_acceptance(acceptance_probability):
+            accepted_steps += 1
+            current_system.wave_function_value = trial_system.wave_function_value
+            for k in range(len(self.initial_system.particles[j].position)):
+                current_system.particles[j].position[k] = trial_system.particles[j].position[k]
+        else:
+            for k in range(len(self.initial_system.particles[j].position)):
+                trial_system.particles[j].position[k] = current_system.particles[j].position[k]
 
         return current_system, accepted_steps
 
